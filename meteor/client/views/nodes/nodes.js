f164b67dbfab40135da3a251d56d22bb1399a931
Levin Keller <post@levinkeller.de>
2014-10-02 13:44:12 +0200

    Restructuring Accounts view

--- meteor/client/views/nodes/nodes.js
+++ meteor/client/views/nodes/nodes.js
@@ -1,18 +1,5 @@
 // on the client
-Template.nodes.helpers({
-  exchanges: function(){
-    return Exchanges.find().fetch();
-  }
-});
 
-Template.nodes.helpers({
-  bankaccounts: function(){
-    return BankAccounts.find().fetch();
-  }
-});
+
+
 // on the client
-Template.nodes.helpers({
-  bitcoinwallets: function(){
-    return BitcoinWallets.find().fetch();
-  }
-});
