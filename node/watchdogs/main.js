2a43c137a0a15f4831a3d299d5d031edac0449b8
Levin Keller <post@levinkeller.de>
2014-12-01 19:09:17 +0100

    Logging only addresses that coyno knows.

--- node/watchdogs/main.js
+++ node/watchdogs/main.js
@@ -1,39 +1,54 @@
-var ws = require('ws');
+var WebSocket = require('ws');
+var MongoDb = require('mongodb');
+var MongoClient = MongoDb.MongoClient;
+var assert = require('assert');
 
 
 
-if (Meteor.isServer) {
-    Meteor.startup(function () {
-        var WebSocket = Meteor.npmRequire('ws');
-        var Fiber = Meteor.npmRequire('fibers');
-        var chainWS = new WebSocket("wss://ws.chain.com/v2/notifications");
+var chainWS = new WebSocket("wss://ws.chain.com/v2/notifications");
 
-        chainWS.on('open', function() {
-            var req = {type: "new-transaction", block_chain: "bitcoin"};
-            chainWS.send(JSON.stringify(req));
+chainWS.on('open', function() {
+    var req = {type: "new-transaction", block_chain: "bitcoin"};
+    chainWS.send(JSON.stringify(req));
+});
+chainWS.on('message', function (data, flags) {
+    var x = JSON.parse(data);
+    if (x.payload.type == "new-transaction") {
+        var outputs = x.payload.transaction.outputs;
+        var inputs = x.payload.transaction.inputs;
+        var inoutputs = inputs.concat(outputs);
+        var addresses  = [];
+        inoutputs.forEach(function (input) {
+             addresses = addresses.concat(input.addresses);
         });
-        chainWS.on('message', Meteor.bindEnvironment(function (data, flags) {
-            var x = JSON.parse(data);
-            var outputs = x.payload.transaction.outputs;
-            var inputs = x.payload.transaction.inputs;
-            var inoutputs = inputs.concat(outputs);
+        // Connection URL
+        var url = 'mongodb://localhost:3001/meteor';
+        // Use connect method to connect to the Server
+        MongoClient.connect(url, function(err, db) {
+            var bitcoinaddresses = db.collection('bitcoinaddresses');
 
-            inoutputs.forEach(function (input) {
-                    var addresses = input.addresses;
-                    addresses.forEach(function (address) {
-                        var knownAddresses = BitcoinAddresses.find({'address': address}).fetch();
-                        if (knownAddresses) {
-                            knownAddresses.forEach(function (address) {
-                                {
-                                    console.log('Received transaction for a user!');
-                                    var wallet = BitcoinWallets.findOne({_id: address.walletId});
-                                    Meteor.call('updateTx4Wallet', wallet);
-                                }
-                            });
-                        }
-                    });
+            bitcoinaddresses.find({ "address": { $in : addresses}}).toArray(function (err, knownAddresses) {
+                if (knownAddresses.length) {
+                    console.dir(knownAddresses.address);
                 }
-            );
-        }, function () { console.log('Failed to bind environment'); }));
-    });
-}
+              db.close();
+            });
+        });
+
+        /*
+         addresses.forEach(function (address) {
+         var knownAddresses = BitcoinAddresses.find({'address': address}).fetch();
+         if (knownAddresses) {
+         knownAddresses.forEach(function (address) {
+         {
+         console.log('Received transaction for a user!');
+         var wallet = BitcoinWallets.findOne({_id: address.walletId});
+         Meteor.call('updateTx4Wallet', wallet);
+         }
+         });
+         }
+         });
+         }
+         );*/
+    }
+});
