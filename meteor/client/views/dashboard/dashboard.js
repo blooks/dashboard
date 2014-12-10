72491a5573e923f5dd0a04a8dcb4a81e7a4d8113
Erasmus Hagen <erasmus@coyno.com>
2014-12-10 18:23:04 +0100

    rmv console logs

--- meteor/client/views/dashboard/dashboard.js
+++ meteor/client/views/dashboard/dashboard.js
@@ -1,17 +1,26 @@
-Template.dashboard.rendered = function(){
 
+
+
+Template.dashboard.rendered = function(){
+//Pie chart for wallet overview 
+function walletData() {
+    var result = [];
+    BitcoinWallets.find({}).forEach(function(wallet){
+        result.push({"label": wallet.label, "value": wallet.balance()});
+    });
+  return result;
+};
 	//Regular pie chart example
 nv.addGraph(function() {
   var areaWidth   = parseInt(d3.select("#chart svg").style("width").replace("px", ""),0);
   var centerWidth = -Math.floor(areaWidth/2);
-
-  console.log(centerWidth);
+  
   var chart = nv.models.pieChart()
       .x(function(d) { return d.label })
       .y(function(d) { return d.value })
       .showLabels(true)
       .labelThreshold(.05)  //Configure the minimum slice size for labels to show up
-      .labelType("percent") //Configure what type of data to show in the label. Can be "key", "value" or "percent"      
+      .labelType("key") //Configure what type of data to show in the label. Can be "key", "value" or "percent"      
       .color(['#9A2158', '#67163B', '#5A1333', '#A7245F', '#4E0928'])
       // Brand primary shades ['#9A2158', '#67163B', '#5A1333', '#A7245F', '#4E0928']
       // Brand secondary shades ['#0C5967', '#0D6574', '#159CB4', '#1CD3F3', '#09434E']
@@ -25,28 +34,23 @@ nv.addGraph(function() {
         ;
     
     d3.select("#chart .nv-legendWrap")
-/*      .attr("transform", "translate(-454,5)");*/
      .attr("transform", function () { return "translate(" + centerWidth + ",10)" ; 
     });
 
   return chart;
 });
-
-
-//Pie chart for wallet overview 
-function walletData() {
-    var result = [];
-    BitcoinWallets.find({}).forEach(function(wallet){
-        result.push({"label": wallet.label, "value": wallet.balance()});
-    });
-  return result;
-}
 };
+
+
 
 // on the client
 Template.dashboard.helpers({
   trades: function(){
     return Trades.find({},{sort: ['date','asc']}).fetch();
+  },
+  showPieChart: function(){
+    return (true);
+    //TODO: @Levin Please make this now show when there is no data.
   }
 });
 
