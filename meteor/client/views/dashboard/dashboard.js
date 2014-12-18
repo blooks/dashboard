9c1d8e141d888aa40b74ec9affdfac90e5d10986
Levin Keller <post@levinkeller.de>
2014-12-18 17:31:07 +0100

    Total balance wrong or not consistent with chart.

--- meteor/client/views/dashboard/dashboard.js
+++ meteor/client/views/dashboard/dashboard.js
@@ -1,78 +1,86 @@
 Template.dashboard.rendered = function(){
 //Pie chart for wallet overview 
-function walletData() {
-    var result = [];
-    BitcoinWallets.find({}).forEach(function(wallet){
-        result.push({"label": wallet.label, "value": wallet.balance()});
-    });
-  return result;
-};
+    function walletData() {
+        var result = [];
+        BitcoinWallets.find({}).forEach(function(wallet){
+            result.push({"label": wallet.label, "value": wallet.balance()});
+        });
+        return result;
+    };
+
 
 // Wallet Pie chart
-nv.addGraph(function() {
-  var areaWidth   = parseInt(d3.select("#pieChart svg").style("width").replace("px", ""),0);
-  var centerWidth = -Math.floor(areaWidth/2);
-  
-  var chart = nv.models.pieChart()
-      .x(function(d) { return d.label })
-      .y(function(d) { return d.value })
-      .showLabels(true)
-      .labelThreshold(.05)  //Configure the minimum slice size for labels to show up
-      .labelType("key") //Configure what type of data to show in the label. Can be "key", "value" or "percent"
-      .color(['#9A2158', '#67163B', '#5A1333', '#A7245F', '#4E0928'])
-      // Brand primary shades ['#9A2158', '#67163B', '#5A1333', '#A7245F', '#4E0928']
-      // Brand secondary shades ['#0C5967', '#0D6574', '#159CB4', '#1CD3F3', '#09434E']
-      // Brand .negative shades ['#FE2C2A', '#B11F1D', '#FE4543', '#981B19', '#711413']
-      ;
+    nv.addGraph(function() {
+        var areaWidth   = parseInt(d3.select("#pieChart svg").style("width").replace("px", ""),0);
+        var centerWidth = -Math.floor(areaWidth/2);
 
-    d3.select("#pieChart svg")
-        .datum(walletData())
-        .transition().duration(450)
-        .call(chart)
+        var chart = nv.models.pieChart()
+                .x(function(d) { return d.label })
+                .y(function(d) { return d.value })
+                .showLabels(true)
+                .labelThreshold(.05)  //Configure the minimum slice size for labels to show up
+                .labelType("key") //Configure what type of data to show in the label. Can be "key", "value" or "percent"
+                .color(['#9A2158', '#67163B', '#5A1333', '#A7245F', '#4E0928'])
+        // Brand primary shades ['#9A2158', '#67163B', '#5A1333', '#A7245F', '#4E0928']
+        // Brand secondary shades ['#0C5967', '#0D6574', '#159CB4', '#1CD3F3', '#09434E']
+        // Brand .negative shades ['#FE2C2A', '#B11F1D', '#FE4543', '#981B19', '#711413']
+            ;
+
+        d3.select("#pieChart svg")
+            .datum(walletData())
+            .transition().duration(450)
+            .call(chart)
         ;
-    
-/*    d3.select("#pieChart .nv-legendWrap")
-     .attr("transform", function () { return "translate(" + centerWidth + ",10)" ; 
-    });
-*/
 
-  nv.utils.windowResize(chart.update);
+        /*    d3.select("#pieChart .nv-legendWrap")
+         .attr("transform", function () { return "translate(" + centerWidth + ",10)" ;
+         });
+         */
 
-  return chart;
-});
+        nv.utils.windowResize(chart.update);
 
-/* Line chart graph */
-
-nv.addGraph(function() {
-    var networthData = Meteor.user().networthData();
-    var chart = nv.models.multiBarChart()
-        .transitionDuration(350)
-        .reduceXTicks(true)
-        .rotateLabels(0)
-        .showControls(true)
-        .groupSpacing(0.1)
-        .margin({top: 30, right: 100, bottom: 50, left: 70});
-
-    chart.xAxis.tickFormat(function(d) {
-      return d3.time.format('%d/%m/%Y')(new Date(d.date))
+        return chart;
     });
 
-    chart.yAxis
-        .tickFormat(function(d) { return d3.format(',.4f')(d) + ' ' +'BTC' });
+    /* Line chart graph */
 
-/*
-    chart.y2Axis
-        .tickFormat(function(d) { return d3.format(".4f")(d) + ' ' +'BTC' });
-*/
-    d3.select('#networthChart svg')
-      .datum(networthData)
-      .call(chart)
-      ;
+    nv.addGraph(function() {
+        var networthData = Meteor.user().networthData();
+        var chart = nv.models.linePlusBarChart()
+            .x(function(d) { return d[0] })
+            .y(function(d) { return d[1] })
+            .margin({top: 30, right: 100, bottom: 50, left: 70});
 
-    nv.utils.windowResize(chart.update);
+        chart.xAxis.tickFormat(function(d){ return d3.time.format('%W-%y')(new Date(d)) });
 
-    return chart;
-});
+        chart.y1Axis.tickFormat(function(d) {return d3.format(',.4f')(d);});
+        chart.y2Axis.tickFormat(function(d) { return d3.format(',.4f')(d);});
+
+        var min = 0;
+        var max = 0;
+        networthData[0].values.forEach(function(valuepair) {
+            min = Math.min(min, valuepair[1]);
+            max = Math.max(max, valuepair[1]);
+        });
+        networthData[1].values.forEach(function(valuepair) {
+            min = Math.min(min, valuepair[1]);
+            max = Math.max(max, valuepair[1]);
+        });
+        chart.bars.forceY([min, max]);
+        chart.lines.forceY([min, max]);
+        /*
+         chart.y2Axis
+         .tickFormat(function(d) { return d3.format(".4f")(d) + ' ' +'BTC' });
+         */
+        d3.select('#networthChart svg')
+            .datum(networthData)
+            .call(chart)
+        ;
+
+        nv.utils.windowResize(chart.update);
+
+        return chart;
+    });
 
 };
 
@@ -80,19 +88,30 @@ nv.addGraph(function() {
 
 // on the client
 Template.dashboard.helpers({
-  trades: function(){
-    return Trades.find({},{sort: ['date','asc']}).fetch();
-  },
-  showPieChart: function(){
-    return (true);
-    //TODO: @Levin Please make this now show when there is no data.
-  }
+    trades: function(){
+        return Trades.find({},{sort: ['date','asc']}).fetch();
+    },
+    showPieChart: function(){
+        return (true);
+        //TODO: @Levin Please make this now show when there is no data.
+    },
+    totalBalance: function (currency)  {
+        //TODO: Remove this. It is redundant to the global helper
+        var saneNumber = function(internalNumber, currency) {
+            if (currency === 'BTC') {
+                return (internalNumber / 100000000).toFixed(8);
+            } else {
+                return (internalNumber / 100000000).toFixed(2);
+            }
+        };
+        return saneNumber(Meteor.user().totalBalance(currency), currency);
+    }
 });
 
 Template.dashboard.events({
-  'click .delete-trade': function(event, template) {
-    return Trades.remove({
-      _id: this._id
-    });
-  }
+    'click .delete-trade': function(event, template) {
+        return Trades.remove({
+            _id: this._id
+        });
+    }
 });
--- meteor/models/accounts.js
+++ meteor/models/accounts.js
@@ -5,33 +5,40 @@ Meteor.users.helpers({
             return (amount/100000000).toFixed(8)
         };
         var balances = [];
-        var incomes = [];
-        var expenses = [];
-        var income = 0;
-        var expense = 0;
+        var changes = [];
         var balance = 0;
+        var change = 0;
         var week = 1357603200000;
         Transfers.find({"details.currency": 'BTC'} , {sort: ['date','asc']}).forEach(function(transfer) {
             while (transfer.date.getTime() >= week) {
-                balances.push({key: 'balance', data: {x: week, y: satoshiToBTC(balance)}});
-                incomes.push({date: week, amount: satoshiToBTC(income)});
-                expenses.push({date: week, amount: satoshiToBTC(expense)});
-                income = 0;
-                expense = 0;
+                balances.push([week, satoshiToBTC(balance)]);
+                changes.push([week, satoshiToBTC(change)]);
+                change = 0;
                 week += 604800000;
             }
             if (transfer.fromExternal()) {
                 balance += transfer.amount();
-                income += transfer.amount();
+                change += transfer.amount();
             }
             if (transfer.toExternal()) {
                 balance -= (transfer.amount() - transfer.fee());
-                expense += (transfer.amount() - transfer.fee());
+                change -= (transfer.amount() - transfer.fee());
             }
         });
-        balances.push({key: 'balance', data: [week, satoshiToBTC(balance)]});
-        incomes.push({date: week, amount: satoshiToBTC(income)});
-        expenses.push({date: week, amount: satoshiToBTC(expense)});
-        return  balances;
+        balances.push([week, satoshiToBTC(balance)]);
+        changes.push([week, satoshiToBTC(change)]);
+        return  [{key: 'Change', bar: true, values: changes}, {key: 'Balance', values: balances}];
+    },
+    totalBalance: function(currency) {
+        var result = 0;
+        Transfers.find({"details.currency": currency}).forEach(function(transfer) {
+            if (transfer.fromExternal()) {
+                result += transfer.amount();
+            }
+            if (transfer.toExternal()) {
+                result -= (transfer.amount() + transfer.fee());
+            }
+        });
+        return result;
     }
 });
