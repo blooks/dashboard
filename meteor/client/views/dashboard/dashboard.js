8a1494ee66dd9efa9ce4dae9824045672d6b223b
Levin Keller <post@levinkeller.de>
2014-12-18 09:58:41 +0100

    Intermediary work.

--- meteor/client/views/dashboard/dashboard.js
+++ meteor/client/views/dashboard/dashboard.js
@@ -18,7 +18,7 @@ nv.addGraph(function() {
       .y(function(d) { return d.value })
       .showLabels(true)
       .labelThreshold(.05)  //Configure the minimum slice size for labels to show up
-      .labelType("key") //Configure what type of data to show in the label. Can be "key", "value" or "percent"      
+      .labelType("key") //Configure what type of data to show in the label. Can be "key", "value" or "percent"
       .color(['#9A2158', '#67163B', '#5A1333', '#A7245F', '#4E0928'])
       // Brand primary shades ['#9A2158', '#67163B', '#5A1333', '#A7245F', '#4E0928']
       // Brand secondary shades ['#0C5967', '#0D6574', '#159CB4', '#1CD3F3', '#09434E']
@@ -45,44 +45,27 @@ nv.addGraph(function() {
 
 nv.addGraph(function() {
     var networthData = Meteor.user().networthData();
-    var chart = nv.models.linePlusBarChart()
-          .margin({top: 30, right: 100, bottom: 50, left: 70})
-          //We can set x data accessor to use index. Reason? So the bars all appear evenly spaced.
-          .x(function(d,i) { return i })
-          .y(function(d,i) {return d[1] })
-      /*
-        TODO need to exclude data from day 1 --> current date where holdings = 0 
-        (to avoid a long time frame of no action) e.g. something like:
-      .filter(function(d){return networthData.values[d][1] !== "0.00000000";})*/          
-          .tooltips(true)
-          .tooltipContent(function(key,i, d) {
-           return '<h3>' + key + '</h3>' +'<span class="btc-value">' + d + '</span>' + '<span class="transaction-date">' + i + '</span>' 
-          });
+    var chart = nv.models.multiBarChart()
+        .transitionDuration(350)
+        .reduceXTicks(true)
+        .rotateLabels(0)
+        .showControls(true)
+        .groupSpacing(0.1)
+        .margin({top: 30, right: 100, bottom: 50, left: 70});
 
     chart.xAxis.tickFormat(function(d) {
-      var dx = networthData[0].values[d] && networthData[0].values[d][0] || 0;
-      return d3.time.format('%d/%m/%Y')(new Date(dx))
+      return d3.time.format('%d/%m/%Y')(new Date(d.date))
     });
 
-    chart.y1Axis
-        .tickFormat(function(d) { return d3.format('.4f')(d) + ' ' +'BTC' })
-        .scale().domain([-2, 2]);
+    chart.yAxis
+        .tickFormat(function(d) { return d3.format(',.4f')(d) + ' ' +'BTC' });
 
+/*
     chart.y2Axis
-        .tickFormat(function(d) { return d3.format(".4f")(d) + ' ' +'BTC' })
-        ;
-
-    /*TODO set range dynamically*/
-    chart.lines.forceY([-2, d3.max(networthData[0].values, function(d) { 
-        console.log("MAX ENTRY IS:" + d);
-        return 2;
-      })]);
-    chart.bars.forceY([-2,2]);
-
+        .tickFormat(function(d) { return d3.format(".4f")(d) + ' ' +'BTC' });
+*/
     d3.select('#networthChart svg')
       .datum(networthData)
-      .transition()
-      .duration(0)
       .call(chart)
       ;
 
