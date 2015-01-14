08bb15e10794a73c459cf3b9502c1a6f7b0686bf
David Grajal <david@tran.sl>
2015-01-14 19:46:31 +0100

    Fixes Username on profile page does not work. #271, Also default username = whatever is before the @ on the registration user email. We don't check for uniqueness, 2 people may have the same username at the moment

--- server/accounts.js
+++ server/accounts.js
@@ -1,4 +1,25 @@
+// DGB 2015-01-14 07:25 This should probably be on the /models
 Meteor.users.deny({remove: function () { return false; }});
+
+// DGB 2015-01-14 07:42 Added this to make possible updates of the profile (Non critical information) directly from
+// the client 
+Meteor.users.allow({
+  update:function(userId, doc, fields, modifier){
+    if((fields[0].indexOf('profile')!==-1) && doc._id===userId){
+      return true;
+    }
+    return false;
+  }
+});
+
 Accounts.onCreateUser(function(options, user) {
+  user.profile = options.profile || {};
+  if(!user.profile.username){
+    if (options.email) {
+      // DGB 2015-01-14 07:28 We need a server side check here to make sure the
+      // username is unique
+      user.profile.username = options.email.match(/[^@]*/i)[0];
+    }
+  }
   return user;
 });
