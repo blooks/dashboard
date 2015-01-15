1646a68d94ade686dde46b4a5e50a6037ea79aec
Levin Keller <levin@coyno.com>
2015-01-15 18:25:57 +0100

    Hot fix.

--- server/publications.js
+++ server/publications.js
@@ -1,7 +1,7 @@
 Meteor.publish("transfers", function (page, numberOfResults) {
   var self = this;
-  var totalAvailableResults = Transfers.find({}).count();
-  var handle = Transfers.find({}, {skip: parseInt(page-1)*parseInt(numberOfResults), limit: numberOfResults, sort: {createdAt: -1}}).observeChanges({
+  var totalAvailableResults = Transfers.find({userId: this.userId}).count();
+  var handle = Transfers.find({userId: this.userId}, {skip: parseInt(page-1)*parseInt(numberOfResults), limit: numberOfResults, sort: {createdAt: -1}}).observeChanges({
     added: function(id, fields){
       fields.totalAvailable = totalAvailableResults;
       self.added("transfers", id, fields);
