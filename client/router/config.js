ed4a2ac1a1dcb8428e34973b99d0292f03234634
lfergon <lfergon@gmail.com>
2015-01-15 10:37:53 +0100

    removed file

---
+++ client/router/config.js
@@ -0,0 +1,20 @@
+Router.configure({
+  layoutTemplate: 'layout',
+  loadingTemplate: 'loading',
+  notFoundTemplate: 'notFound',
+  yieldTemplates: {
+    header: {
+      to: 'header'
+    },
+    sidebar: {
+      to: 'sidebar'
+    },
+    footer: {
+      to: 'footer'
+    }
+  },
+  onBeforeAction: function() {
+    $('meta[name^="description"]').remove();
+    this.next();
+  }
+});
--- packages/coynverter/coynverter.js
+++ packages/coynverter/coynverter.js
@@ -13,7 +13,7 @@ Coynverter = (function() {
    * @param {Date} date
    * @returns {Number}
    */
-  var exchangeRate = function( from, to, date ) {
+  var exchangeRate = function ( from, to, date ) {
 
     // TODO: usage scenarios? - decide if it'll throw errors or return nulls
 
