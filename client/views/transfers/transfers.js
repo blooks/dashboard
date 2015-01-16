8681d053fa8d1eb4646dff1abb48883717c11f73
Levin Keller <levin@coyno.com>
2015-01-16 11:00:23 +0100

    Showing template when no transfers present.

--- client/views/transfers/transfers.js
+++ client/views/transfers/transfers.js
@@ -1,9 +1,3 @@
 Template.transfers.rendered = function () {
   $('[data-toggle="tooltip"]').tooltip();
 };
-
-Template.transfers.helpers({
-  noData: function () {
-    return !Meteor.user().profile.hasTransfers;
-  }
-});
