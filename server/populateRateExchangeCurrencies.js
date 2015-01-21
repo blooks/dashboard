f239801487cae30aaf54322751e13b515a1675f1
lfergon <lfergon@gmail.com>
2015-01-21 22:44:38 +0100

    Modified files

--- server/populateRateExchangeCurrencies.js
+++ server/populateRateExchangeCurrencies.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // BitcoinExchangeRates = new Mongo.Collection('bitcoinExchangeRates');
 // Meteor.startup(function () {
 //   if(BitcoinExchangeRates.find().fetch().length===0){
@@ -14,19 +13,4 @@
 //     });
 //   }
 // });
-=======
-Meteor.startup(function () {
-  if(BitcoinExchangeRates.find().fetch().length===0){
-    var currencies = ["EUR", "USD"];
-    var Coynverter = Meteor.npmRequire("coyno-converter");
-    var coynverter = new Coynverter();
-    currencies.forEach(function (currency) {
-      coynverter.getExchangeRatesForNewCurrency("meteor", currency, "bitcoinExchangeRates", function (err, result) {
-        if(err){
-          Log.error(err);
-        }
-      });
-    });
-  }
-});
->>>>>>> develop
+
