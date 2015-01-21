071e645effe7bdeb0d2e3de8d51c633994b47545
David Grajal <david@tran.sl>
2015-01-21 21:14:02 +0100

    Fixes #351 New Wallet UI + Input wallet data fields for new wallet

--- client/views/nodes/newBitcoinWallet.js
+++ client/views/nodes/newBitcoinWallet.js
@@ -10,14 +10,35 @@ Template.newBitcoinWallet.helpers({
   },
   getNewWalletData: function(s) {
     return Template.instance().newWalletData.get();
+  },
+  getLogoImageFromWalletType: function(s) {
+    if (s==='Single Address') return '/img/wallet-icon-default.png' 
+    return '/img/external-logos/' + s.replace(/\s/g, '-').toLowerCase() + '-logo.png';
+  },
+  isElectrum: function() {
+    return (this.type==='Electrum')
+  },
+  isBitcoinWallet: function() {
+    return (this.type==='Bitcoin Wallet')
+  },
+  getWalletFormatFromWalletType: function(s) {
+    // DGB 2015-01-21 08:33 We need to think of a more elegant way to make this
+    // less fragile, otherwise a change on the config.coffee file may break the
+    // site
+    if (s==='Bitcoin Wallet') return 'BIP32';
+    if (s==='Single Address') return 'Single Addresses';
+    return s; // DGB 2015-01-21 08:25 True for Armory & Electrum
   }
 });
 
 Template.newBitcoinWallet.events({
+  'click .back': function (event,template) {
+     template.newWalletStage.set('first');
+  },
   'click .wallet-thumbs': function (event,template) {
     // DGB 2015-01-20 06:57 There are better ways to get this data
     var wallet=$($(event.currentTarget).children()[1]).html()
-    if (!(['Armory','Electrum','Bitcoin Wallet','Electrum'].indexOf(wallet) === -1)) {
+    if (!(['Armory','Electrum','Bitcoin Wallet','Electrum','Single Address'].indexOf(wallet) === -1)) {
       template.newWalletData.set({type:wallet});
       template.newWalletStage.set('second');
     }
