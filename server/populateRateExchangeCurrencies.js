e58a4ac7e61d3fbbb2eded89bf84b23f8b2f696d
lfergon <lfergon@gmail.com>
2015-01-22 18:43:42 +0100

    Solved problem with mocha restarting server

--- server/populateRateExchangeCurrencies.js
+++ server/populateRateExchangeCurrencies.js
@@ -2,15 +2,18 @@ BitcoinExchangeRates = new Mongo.Collection('bitcoinExchangeRates');
 Meteor.startup(function () {
   if(BitcoinExchangeRates.find().fetch().length===0){
     var currencies = ["EUR", "USD"];
-    var Coynverter = Meteor.npmRequire("coyno-converter");
-    var coynverter = new Coynverter();
-    currencies.forEach(function (currency) {
-      coynverter.getExchangeRatesForNewCurrency("meteor", currency, "bitcoinExchangeRates", function (err, result) {
-        if(err){
-          Log.error(err);
-        }
+    //22.01.2015 LFG this must be moved to a meteor package Mocha test try to populate the database every time that the server is restarted
+    if(process.env.MONGO_URL.indexOf("mocha")===-1){
+      var Coynverter = Meteor.npmRequire("coyno-converter");
+      var coynverter = new Coynverter(process.env.MONGO_URL);
+      currencies.forEach(function (currency) {
+        coynverter.getExchangeRatesForNewCurrency(currency, "bitcoinExchangeRates", function (err, result) {
+          if(err){
+            Log.error(err);
+          }
+        });
       });
-    });
+    }
   }
 });
 
