f3959769b45d09b55bab5644e0116b13c79db330
Erasmus Hagen <erasmus@coyno.com>
2015-02-12 18:55:56 +0100

    Wallet details opens in modal with a drop down functionality for adding single addresses

--- client/views/nodes/bitcoin/addBitcoinAddress.js
+++ client/views/nodes/bitcoin/addBitcoinAddress.js
@@ -1,10 +1,8 @@
 Template.addBitcoinAddress.events({
-});
-Template.addBitcoinAddress.events({
-  'click .bitcoin-address-add-toggle': function () {
+  'click .bitcoin-add-address-toggle': function () {
     var targetID = "#" + this._id;
-    jQuery(targetID + " .bitcoin-address-add").slideToggle(400, function () {
-      jQuery(targetID + " .data-toggle")
+    jQuery(targetID + " .bitcoin-add-address").slideToggle(400, function () {
+      jQuery(targetID + " .toggle-button")
         .toggleClass('icon-up-open-big')
         .toggleClass('icon-down-open-big');
     });
