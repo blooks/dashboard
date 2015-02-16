bb21498c99c285c6b380e4af56694fa620758ea0
Levin Keller <levin@coyno.com>
2015-02-16 14:33:41 +0100

    Removed not working compound unique but sparse index on mongo.

--- models/bitcoinwallets.js
+++ models/bitcoinwallets.js
@@ -221,9 +221,6 @@ BitcoinWallets.before.remove(function (userId, doc) {
 
 if (Meteor.isServer) {
 
-
-  BitcoinWallets._ensureIndex({userId: 1, superNode: 1}, {sparse: true, unique: true});
-
   BitcoinWallets.after.remove(function (userId, doc) {
     var oneTransfer = Transfers.findOne({'userId': doc.userId});
     if (!oneTransfer) {
