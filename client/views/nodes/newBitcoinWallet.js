3ed7e1b51ad6395243040a25c7af48f5878a2dbe
Levin Keller <levin@coyno.com>
2015-03-18 17:56:58 +0100

    Removing unused packages.

--- client/views/nodes/newBitcoinWallet.js
+++ client/views/nodes/newBitcoinWallet.js
@@ -1,11 +1,3 @@
-Template.newBitcoinWallet.created = function() {
-  this.newWalletStage = new ReactiveVar('first','');
-  this.newWalletData = new ReactiveVar(false,'');
-  this.newWalletFormValid = new ReactiveVar(false,'');
-  this.newWalletFormValidLabel = new ReactiveVar(false,'');
-  this.newWalletFormValidSeed = new ReactiveVar(false,'');
-};
-
 Template.newBitcoinWallet.helpers({
   availableWallets: function () {
     return Meteor.settings.public.coyno.availableWallets;
