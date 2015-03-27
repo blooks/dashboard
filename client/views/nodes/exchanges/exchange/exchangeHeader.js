5f9c1e13f037e684247c27120e24be55db1d8b04
Levin Keller <levin@coyno.com>
2015-03-27 14:01:45 +0100

    Adding logic.

--- client/views/nodes/exchanges/exchange/exchangeHeader.js
+++ client/views/nodes/exchanges/exchange/exchangeHeader.js
@@ -3,3 +3,15 @@ Template.exchangeHeader.helpers({
     return "nodes.exchanges.type." + this.exchange;
   }
 });
+
+
+Template.exchangeHeader.events({
+  'click .delete-exchange': function () {
+    return Exchanges.remove({
+      _id: this._id
+    });
+  },
+  'click .update-exchange': function () {
+    return this.update();
+  }
+});
