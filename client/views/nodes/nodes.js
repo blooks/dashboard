0ef0f2d77974b97dc87d70812e8610daa43e6c5e
Levin Keller <levin@coyno.com>
2015-04-09 19:09:55 +0200

    Fixing Logo on exchanges view.

--- client/views/nodes/nodes.js
+++ client/views/nodes/nodes.js
@@ -2,5 +2,11 @@
 Template.nodes.helpers({
   heading: function () {
     return 'nodes.headings.' + this.type;
+  },
+  iconType: function() {
+    switch (this.type) {
+      case 'bitcoinWallets': return 'wallet';
+      case 'exchanges' : return 'exchange';
+    }
   }
 });
