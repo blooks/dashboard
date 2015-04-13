d879f622a40365dbf259353d7ee2354e3a9f9bd8
Levin Keller <levin@coyno.com>
2015-04-13 18:54:54 +0200

    Nicer pagination menu.

--- server/publications.js
+++ server/publications.js
@@ -41,6 +41,7 @@ Meteor.publish('exchanges', function() {
     Levin:
     Very important to not share the credentials with the user. They might contain very
     sensible and powerful data like accessTokens for oauth applications.
+    TODO: Move credentials to server side collection.
    */
   return Exchanges.find({userId: this.userId}, {fields: {credentials : 0} });
 });
